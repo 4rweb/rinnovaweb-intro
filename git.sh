8d8a4cde229ba844151267a1795dde278c47c930
wiliamdeschepper <wiliam@4rweb.net>
2022-05-11 00:28:11 -0300

    first commit

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# rinnovaweb-intro" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/4rweb/rinnovaweb-intro.git
+git push -u origin main
